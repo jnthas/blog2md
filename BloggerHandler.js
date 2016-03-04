b35b4509c66e7a1335f9ad06b2f4b372822b1f76
Jonathas Amaral <jnthas@gmail.com>
2016-03-04 00:52:06 -0300

    extracting data and saving markdown files.

--- BloggerHandler.js
+++ BloggerHandler.js
@@ -1,3 +1,7 @@
+var slug = require('slug');
+var exporter = require('./exporter.js');
+var toMarkdown = require('to-markdown');
+
 function BloggerHandler() {};
 
 BloggerHandler.prototype.handle = function (data) {
@@ -6,6 +10,37 @@ BloggerHandler.prototype.handle = function (data) {
 		console.log('---------------------');
 		console.log('Author: ', data.feed.author[0].name.$t);
 		console.log('Blog Title: ', data.feed.title.$t);
+
+
+		var divConverter = {
+			filter: 'div',
+			replacement: function (content) {
+				return content;
+			}
+		};
+
+		data.feed.entry.forEach(function (entry) {
+			var post = {
+				title: entry.title.$t,
+			  	date: entry.published.$t,
+			  	updated: entry.updated.$t,
+			  	tags: joinTags(entry.category),
+			  	permalink: slug(entry.title.$t, {lower: true}),
+			  	content: toMarkdown(entry.content.$t, {converters: [divConverter]})
+			};
+
+			exporter.export(post);
+			
+		});
 };
+
+
+function joinTags (tags) {
+	var t = '';
+	tags.forEach(function (tag) {
+		t += ',' + tag.term;
+	});
+	return t.substring(1);
+}
 
 module.exports = new BloggerHandler();
--- exporter.js
+++ exporter.js
@@ -3,6 +3,7 @@ var hogan = require("hogan.js");
 var templateFile = fs.readFileSync("templates/post.mustache");
 var template = hogan.compile(templateFile.toString());
 
+/*
 var post = {
   title: 'Introdução a Engenharia de Software',
   date: '02/05/2016',
@@ -11,5 +12,20 @@ var post = {
   permalink: 'intro-engenharia-software',
   content: '<h1>Introdução a Engenharia de Software<h1>'
 };
+*/
 
-console.log(template.render(post));
+
+exports.export = function (entry) {
+	var post = template.render(entry);
+	return writeFile('./posts/' + entry.permalink + '.md', post);
+};
+
+function writeFile (fileName, data) {
+	fs.writeFile(fileName, data, function (err) {
+		if (err) {
+			throw err;
+		}
+		return true;
+	});
+
+}
